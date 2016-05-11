bb99b3df3f7202baea0defafaa34bc24c8149069
barat <guillaume.baratier@live.fr>
2016-05-11 11:21:44 +0200

    TeamDAO est arrivé!

---
+++ src/java/DAO/TeamDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import javax.ejb.Stateless;
+import Entity.Team;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+/**
+ *
+ * @author Guillaume
+ */
+@Stateless
+public class TeamDAO {
+    @PersistenceContext(unitName="ManageMyProjectPU") 
+    private EntityManager em;
+ 
+    // Stores a new guest: 
+    public void persist(Team team) {
+        em.persist(team);
+    }
+ 
+    // Retrieves all the users:
+    public List<Team> getAllSubjects() {
+        TypedQuery<Team> query = em.createQuery(
+            "SELECT g FROM Subject g ORDER BY g.id", Team.class);
+        return query.getResultList();
+    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
